8edd892e2a3c682bb262b2d67900341b1d61da38
kshatlov <kshatlov@gmail.com>
2016-02-24 12:46:18 +0600

    add cmake for vs2005

---
+++ build_vs2005.bat
@@ -0,0 +1,7 @@
+mkdir build
+mkdir "build/vs2005"
+pushd "build/vs2005"
+cmake -G"Visual Studio 8 2005" ../../  -DDIST_WIN32VS05=1 -DDIST_WINDOWS=1
+
+"%VS80COMNTOOLS%\..\IDE\devenv.com"  Project.sln /Build
+popd 
