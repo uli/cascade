f8bb3f2924deace6d5131e14fb45940318e36e65
kshatlov <kshatlov@gmail.com>
2016-02-24 18:13:22 +0600

    build release

--- build_vs2005.bat
+++ build_vs2005.bat
@@ -3,5 +3,5 @@ mkdir "build/vs2005"
 pushd "build/vs2005"
 cmake -G"Visual Studio 8 2005" ../../  -DDIST_WIN32VS05=1 -DDIST_WINDOWS=1
 
-"%VS80COMNTOOLS%\..\IDE\devenv.com"  Project.sln /Build
+"%VS80COMNTOOLS%\..\IDE\devenv.com"  Project.sln /build Release
 popd 
